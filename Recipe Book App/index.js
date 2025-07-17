082aacca9fabaf675cdc3a8e21236ae2f35b3a41
Hadeer Salah <hadeersalah127@gmail.com>
2025-07-17 15:14:01 +0300

    creating elements in the JS file

--- Recipe Book App/index.js
+++ Recipe Book App/index.js
@@ -1,3 +1,22 @@
 const API_KEY = "275d58779ccf4e22af03e792e8819fff";
 const recipeUL = document.querySelector("#recipe-list");
 
+const dispalyRecipes = (recipes) => {
+    recipeUL.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl.classList.add("recipe-item");
+        const recipeItemImageEl = document.createElement("img");
+        recipeItemImageEl.src = recipe.image;
+        recipeItemImageEl.alt = "recipe-image";
+
+        const recipeTitleEl = document.createElement("h2");
+        recipeTitleEl.innerText = recipe.title;
+
+        const recipeIngredient = document.createElement("p")
+        recipeIngredient.innerHTML = `<strong>Ingredients:</strong>${recipe.extendIngredients.map((ingredient) =>
+            ingredient.original.join(", ")
+        )}`
+
+    });
+};
